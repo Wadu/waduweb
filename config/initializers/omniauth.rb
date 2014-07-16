5cccb2b5a4279642c7fda15e3e96222b38d3b192
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-07-16 01:04:07 -0300

    add fb login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], 
-           {:scope => 'email, publish_stream, user_friends, user_events', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+           {:scope => 'email, publish_stream, user_friends, user_events', :client_options => {:ssl => {:verify => false}}}
 end
