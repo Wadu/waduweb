b47c3db207e9d7f1149e5840016763a01449df73
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-07-16 00:07:26 -0300

    fix fb login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.development?
-    provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  else
-    provider :heroku, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  end
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
