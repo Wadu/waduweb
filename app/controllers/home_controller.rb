347b2f891dbcfaaf96b294e8b1c42b8dfe2941a5
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-07-16 02:00:27 -0300

    add photo profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @graph = Koala::Facebook::API.new(current_user.oauth_token)
+    @graph = Koala::Facebook::API.new(current_user.oauth_token) if current_user
   end
 end
