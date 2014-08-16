3a5c3bc09b56b7672def879d4b946af6142d6a67
Sergio Sebastian Arriagada <sergio@nibyru.com>
2014-08-16 17:51:48 -0300

    fix geocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,20 @@
 //= require angular
 //= require angular-resource
 //= require wadu-app
+//= require maps-app
+//= require jquery.geocomplete.min
+//= require facebook
+//= require geocomplete
+
+$(function() {
+  initialize_map();
+  initialize_geocomplete();
+});
+
+$(document).on('page:load', function() {
+  return $('[ng-app]').each(function() {
+    var module;
+    module = $(this).attr('ng-app');
+    return angular.bootstrap(this, [module]);
+  });
+});
--- app/assets/javascripts/footer-application.js
+++
@@ -1,9 +0,0 @@
-//= require maps-app
-//= require facebook
-//= require geocomplete
-
-$(function() {
-  initialize_map();
-  initialize_geocomplete();
-});
-
